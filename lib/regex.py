d0fa99d7b4b2449804a748237bf35207a2a70daa
matthew rendely <matthew.rendely@gmail.com>
2023-06-19 18:15:35 -0700

    done

--- lib/regex.py
+++ lib/regex.py
@@ -1,5 +1,15 @@
 import re
 
-my_pattern = r""
+my_pattern = r"\b[A-z|'| |,]+[y|\.|h|\?]{2}"
 my_regex = re.compile(my_pattern)
 
+FINDALL_STRING = """
+    It's such a lovely day today.
+    Where'd all the time go?
+    Some weather we're having today, huh?
+    Tomorrow never knows!
+    Maybe today's just not my day.
+    It's clobbering time!
+"""
+
+print(my_regex.findall(FINDALL_STRING))
